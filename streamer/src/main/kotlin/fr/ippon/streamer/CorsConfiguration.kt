1836cce9667fb6ccaefe50ea3cbd5c87c270726c
Paul Boutes <paul.boutes@gmail.com>
2018-03-05 04:05:17 +0100

    feat(streamer): add cors + functional router

---
+++ streamer/src/main/kotlin/fr/ippon/streamer/CorsConfiguration.kt
@@ -0,0 +1,28 @@
+package fr.ippon.streamer
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
+@Configuration
+class CorsConfiguration : WebFilter {
+    override fun filter(ctx: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        ctx.response.headers.add("Access-Control-Allow-Origin", "*")
+        ctx.response.headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+        ctx.response.headers.add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
+        if (ctx.request.method == HttpMethod.OPTIONS) {
+            ctx.response.headers.add("Access-Control-Max-Age", "1728000")
+            ctx.response.statusCode = HttpStatus.NO_CONTENT
+            return Mono.empty()
+        } else {
+            ctx.response.headers.add("Access-Control-Expose-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
+            return chain.filter(ctx)
+        }
+    }
+
+
+}
