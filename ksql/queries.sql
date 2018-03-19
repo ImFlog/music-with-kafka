9ee94de1104bc20403c934e3810c5ee6b4595d04
ImFlog <garcia.florian.perso@gmail.com>
2018-03-19 17:28:16 +0100

    Working example

--- ksql/queries.sql
+++ ksql/queries.sql
@@ -1,3 +1,11 @@
+-- Reset streams and tables
+DROP STREAM twitter_foo;
+DROP TABLE user_tweet_count;
+DROP STREAM category_tweets;
+DROP STREAM twitter_rekeyed;
+DROP STREAM twitter_clean;
+DROP STREAM twitter_raw;
+
 -- Set properties
 SET 'auto.offset.reset' = 'earliest';
 
@@ -5,39 +13,67 @@ SET 'auto.offset.reset' = 'earliest';
 CREATE STREAM twitter_raw (CreatedAt BIGINT, Id BIGINT, Text VARCHAR, User VARCHAR) \
 WITH (KAFKA_TOPIC='twitter_json', VALUE_FORMAT='JSON');
 
+-- Show stream content
+SELECT * FROM twitter_raw;
+-- Stream is infinite by definition, CTRL+C to stop.
+
 -- Create a derived stream with user values & text only
 CREATE STREAM twitter_clean AS \
 SELECT TIMESTAMPTOSTRING(CreatedAt, 'yyyy-MM-dd HH:mm:ss.SSS') AS CreatedAt,\
-    EXTRACTJSONFIELD(User,'$.Name') AS user_name,\
-    EXTRACTJSONFIELD(User,'$.ScreenName') AS user_screenName,\
-    EXTRACTJSONFIELD(User,'$.ProfileImageURL') AS user_image,\
+    EXTRACTJSONFIELD(User,'$.Name') AS Name,\
+    EXTRACTJSONFIELD(User,'$.ScreenName') AS ScreenName,\
+    EXTRACTJSONFIELD(User,'$.ProfileImageURL') AS Image,\
     Text \
 FROM twitter_raw;
 
--- Create a derived stream with user_screenname | category
+-- Clean version print but limit to end the query
+SELECT * FROM twitter_clean LIMIT 10;
+
+-- We can start to play, count tweets per user
+CREATE TABLE user_tweet_count AS \
+SELECT ScreenName, COUNT(*) AS Count \
+FROM twitter_clean \
+GROUP BY ScreenName;
+
+-- Show data
+SELECT ScreenName, Image FROM user_tweet_count LIMIT 10;
+
+-- Join example : Melody tweets per user 
+-- We need to force PARTITION BY https://github.com/confluentinc/ksql/issues/749
 CREATE STREAM category_tweets AS \
 SELECT \
-    user_screenname, \
-    (Text LIKE '%drum%') AS IS_DRUM, \
-    (Text LIKE '%melody%') AS IS_MELODY, \
-    (Text like '%heavy%') AS IS_HEAVY_BASS, \
-    (Text like '%lead%') AS IS_LEAD_BASS, \
-    (Text like '%line%') AS IS_LINE_BASS, \
-    (Text like '%pad%') AS IS_PAD, \
-    (Text like '%synth%') AS IS_SYNTH, \
-    (Text like '%vocal%') AS IS_VOCAL, \
+    ScreenName, \
+    (Text LIKE '%drum%') AS IsDrum, \
+    (Text LIKE '%melody%') AS IsMelody, \
+    (Text like '%heavy%') AS IsHeavyBass, \
+    (Text like '%lead%') AS IsLeadBass, \
+    (Text like '%line%') AS IsLineBass, \
+    (Text like '%pad%') AS IsPad, \
+    (Text like '%synth%') AS IsSynth, \
+    (Text like '%vocal%') AS IsVocal, \
     Text \
-FROM twitter_clean;
-
--- Count tweets per user
-CREATE TABLE user_tweet_count AS \
-SELECT user_screenName, COUNT(*) AS count \
 FROM twitter_clean \
-GROUP BY user_screenname HAVING COUNT(*) > 1;
+PARTITION BY ScreenName;
 
--- Join between user tweets and categories INNER IS NOT SUPPORTED
--- TODO : this doesn't work.
-SELECT utc.screenname, utc.count, count(cat.is_drum) \
+-- Actual JOIN query
+SELECT \
+    cat.ScreenName AS ScreenName, \
+    Max(utc.Count) AS TotalCount, \
+    COUNT(cat.isMelody) AS MelodyCount \
 FROM category_tweets cat \
-LEFT JOIN user_tweet_count utc ON cat.user_screenname = utc.user_screenName \
-WHERE utc.count > 10;
+LEFT JOIN user_tweet_count utc ON cat.ScreenName = utc.ScreenName \
+WHERE cat.IsMelody = true \
+AND utc.Count > 10 \
+GROUP BY cat.ScreenName;
+
+-- RECREATE charts like processor
+-- Workaround for mandatory GROUP BY : https://github.com/confluentinc/ksql/issues/430
+CREATE STREAM twitter_foo AS \
+SELECT 1 AS FOO, * FROM twitter_clean;
+
+-- Default commit interval is 10sec
+SELECT TIMESTAMPTOSTRING(ROWTIME, 'yyyy-MM-dd HH:mm:ss.SSS') AS WINDOW_START, ScreenName, COUNT(*) \
+FROM twitter_foo \
+WINDOW TUMBLING (SIZE 30 SECONDS) \
+WHERE Text LIKE '%drum%' \
+GROUP BY FOO, ScreenName;
